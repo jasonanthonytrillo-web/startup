8fed6d6aa18d85d3b7c23538b96e47d01636b1a6
asdasdasd1213267 <github1437@gmail.com>
2026-04-21 22:54:43 +0800

    docker

---
+++ backend/docker/start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Seed the database if needed, otherwise just migrate
+php artisan migrate --force
+
+# Optimize
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# Start Apache
+apache2-foreground
